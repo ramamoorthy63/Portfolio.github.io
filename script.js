965ad3efb5bd4118467c2062599b8ecb761a6285
ramamoorthy63 <152957384+ramamoorthy63@users.noreply.github.com>
2024-01-31 23:27:38 +0530

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+/* ----- TYPING EFFECT ----- */
+let typingEffect = new Typed(".typedText",{
+    strings : ["Sri Ram","Web Developer","Web Designer"],
+    loop : true,
+    typeSpeed : 110, 
+    backSpeed : 100,
+    backDelay : 1000,
+  })
